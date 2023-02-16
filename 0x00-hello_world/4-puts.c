16b1393bf1475878f75ddabf9597920679bb3245
progdee <dhebhyofummite1@yahoo.com>
2023-02-16 04:00:20 -0800

    mandatory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like buiding a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but th eoucome is a piece of art,\n");
+	return (0);	
+} 
