d9e2f87faae6dff063a017c92413a46961eb6a54
progdee <dhebhyofummite1@yahoo.com>
2023-02-21 08:20:48 -0800

    optionals

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table
+ * @n: a number
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - list natural numbers
+ * Return: 0
+ */
+int main(void)
+{
+
+	return (0);
+}
