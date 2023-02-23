790c0bb619799324da29f5f8b7cc79c0b40d5e59
progdee <dhebhyofummite1@yahoo.com>
2023-02-23 04:09:56 -0800

    mandatory

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: a number
+ * Return: 1 and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c < '10)
+		return (1);
+
+	else
+		return (0);
+}
