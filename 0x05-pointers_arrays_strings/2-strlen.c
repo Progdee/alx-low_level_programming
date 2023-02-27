0b2d4ee0a74901080ff918ad341d6b43d19cde01
progdee <dhebhyofummite1@yahoo.com>
2023-02-27 05:50:26 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @c: character
+ * Return: c
+ */
+
+int _strlen(char *s)
+{
+	int c;
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		;
+	}
+	return (c);
+}
