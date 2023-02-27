f87225c2c8ee52a6e3c1bb1fa0b71f8b93113741
progdee <dhebhyofummite1@yahoo.com>
2023-02-27 07:07:10 -0800

    task 8

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src...
+ * @dest: points to 
+ * @src: pointed to
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
