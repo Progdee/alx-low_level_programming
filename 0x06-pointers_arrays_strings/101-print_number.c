d26e8506efadecefbb2ab1651be111f76eca6361
progdee <dhebhyofummite1@yahoo.com>
2023-03-02 02:00:29 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,6 +2,7 @@
 
 /**
  * print_number - function to print the exact number
+ *
  * @n: n of type int
  */
 
@@ -9,14 +10,16 @@ void print_number(int n)
 {
 	unsigned int digit = n;
 
+	/*first check if its negative*/
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		digit = -digit;
 	}
 
+	/* recursively print the first few digits*/
 	if ((digit / 10) > 0)
 		print_number(digit / 10);
 
-	putchar((digit % 10) + '0');
+	_putchar((digit % 10) + '0');
 }
