7cb737d67e9e6b25500eecbd73c8d381bb66fe0d
progdee <dhebhyofummite1@yahoo.com>
2023-03-02 01:43:00 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ * Return: the final string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.',
+					'!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
