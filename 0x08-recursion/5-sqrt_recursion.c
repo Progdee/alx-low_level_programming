44c7ea3957f24b284f88769c78f4c5aa51532708
progdee <dhebhyofummite1@yahoo.com>
2023-03-08 03:13:01 -0800

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,34 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number
- * Return: -1 if n does not have a natural square root
+ * power_operation - returns the natural square root of a number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root or -1.
  */
-
-int _sqrt_recursion(int n)
+int power_operation(int n, int c)
 {
-	if (sqrt * sqrt == num)
-		return (sqrt);
-	if (sqrt * sqrt > num)
-		return (-1);
-	else
-		return (helper(num, sqrt + 1));
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
 }
-
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else
-		return (helper(n, 0));
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- * is_prime_number - check if the input integer is a prime number
- * @n: number input
- * Return: 1 or 0
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-
-int is_prime_number(int n)
+int is_prime(unsigned int n, unsigned int c)
 {
-	if (i < num)
+	if (n % c == 0)
 	{
-		if (num % i == 0)
-			return (0);
+		if (n == c)
+			return (1);
 		else
-			return (helper(num, i + 1));
+			return (0);
 	}
-	else
-		return (1);
-
+	return (0 + is_prime(n, c + 1));
 }
-
 /**
-* is_prime_number - checks whether n is prime number
-* @n: to be checked;
-*
-* Return: returns 1 if prime, 0 if not
-*/
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n == 0)
 		return (0);
-	else
-		return (helper(n, 2));
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
 }
