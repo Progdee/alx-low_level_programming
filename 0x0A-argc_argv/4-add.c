9e4e12920d19f45dccbf9a4e231012414a4c5f15
progdee <dhebhyofummite1@yahoo.com>
2023-03-10 04:26:21 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - adds positive numbers
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 if no number is passed, 1 if one of the numbers contains symbols
+ * Return: 0 if no number is passed, 1 if one of the nu...
  */
 
-int main(int agc, char **argv)
+int main(int agc, char *argv[])
 {
 	int sum = 0, count = 1, i;
 
-	if (argc -- 1)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
@@ -20,7 +22,7 @@ int main(int agc, char **argv)
 	{
 		for (i = 0; argv[count][i] != "\0"; i++)
 		{
-			if (!(isdigit (argv[count][i])))
+			if (!(isdigit(argv[count][i])))
 			{
 				printf("Error\n");
 				return (1);
