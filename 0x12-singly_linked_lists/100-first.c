03d8f902bea2fce6548f7e2a7cc6cfa37b300f73
progdee <dhebhyofummite1@yahoo.com>
2023-03-30 14:13:41 -0700

    singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -14,6 +14,6 @@ void start_up_func(void)__attribute__((constructor));
 
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
