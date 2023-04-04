36b7ddf7d1973225629ae0aa3581936d964fce16
progdee <dhebhyofummite1@yahoo.com>
2023-04-04 15:16:05 -0700

    mandatory

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * print_listint - prints all the elements of a linked list
  * @h: head of the list
  *
  * Return: the number of nodes
